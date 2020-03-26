5a61dfead146a9eaf5387782627ed02f335da61d
sandralannes <sandra.lannes1@gmail.com>
2020-03-26 20:55:33 +0200

    kommentaarid

--- src/Mäng.java
+++ src/Mäng.java
@@ -2,13 +2,25 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Mäng {
+    //mängutabel
     static String[][] tabel = new String[6][7];
 
+    /**
+     * Meetodi abil valitakse loosiga kumb mängija alustab.
+     *
+     * Parameetrid puuduvad.
+     * Meetod ei tagasta midagi.
+     */
     public static boolean valiAlustaja() {
         return Math.random() < 0.5;
     }
 
-
+    /**
+     * Meetodi abil täidetakse tühi tabel "-"-ga.
+     *
+     * Parameetrid puuduvad.
+     * Meetod ei tagasta midagi.
+     */
     public static void täidaTabel() {
         for (int i = 0; i < tabel.length; i++) {
             String[] rida = tabel[i];
@@ -16,6 +28,12 @@ public class Mäng {
         }
     }
 
+    /**
+     * Meetodi abil tehakse käik.
+     *
+     * @param märk Esimese või teise mängija märk mida hakatakse tabelisse lisama.
+     * Meetod ei tagasta midagi.
+     */
     public static void käik(String märk) {
         valjasta_tabel();
         boolean käikTehtud = false;
@@ -31,21 +49,29 @@ public class Mäng {
                         käikTehtud = true;
                         break;
                     } else if (i == 0) {
+                        //kui rida on juba täis.
                         System.out.println("See rida on juba täis!!!");
                         valjasta_tabel();
                         break;
                     }
                 }
             } else if (käikTehtud == false) {
+                //kui sellise indeksiga rida ei leidu.
                 System.out.println("See rida ei sobi!!!");
                 valjasta_tabel();
             }
         }
     }
 
-    //See kes alustab, lisab X-e.
-    //Teine mängija mängib O-dega.
+
+    /**
+     * Meetod kontrollib, kas mõnes veerus, reas või diagonaalis on neli samasugust sümbolit reas.
+     *
+     * Parameetrid puuduvad.
+     * Meetod tagastab 1, kui esimene mängija on võitnud, 2 kui teine mängija on võitnud ja 0 kui kumbki mängija pole veel võitnud.
+     */
     public static int kasNeliReas() {
+        //ridade kontroll
         for (int i = tabel.length - 1; i > 0; i--) {
             String[] rida = tabel[i];
             String sõne = String.join("", rida);
@@ -55,6 +81,7 @@ public class Mäng {
                 return 2;
             }
         }
+        //veergude kontroll
         for (int i = 0; i < tabel[0].length; i++) {
             String[] veerg = new String[tabel.length];
             for (int j = 0; j < tabel.length; j++) {
@@ -67,9 +94,15 @@ public class Mäng {
                 return 2;
             }
         }
+        //diagonaalide kontroll
         return kontrollidiagonaalid();
     }
-
+    /**
+     * Meetod kontrollib, kas mõnes diagonaalis on neli samasugust sümbolit reas.
+     *
+     * Parameetrid puuduvad.
+     * Meetod tagastab 1, kui esimene mängija on võitnud, 2 kui teine mängija on võitnud ja 0 kui kumbki mängija pole veel võitnud.
+     */
     public static int kontrollidiagonaalid() {
         // ühtepidi diagonaalide kontroll
         for (int i = 3; i < tabel.length; i++) {
@@ -93,7 +126,12 @@ public class Mäng {
         }
         return 0;
     }
-
+    /**
+     * Meetod väljastab mängutabeli hetkeseisu.
+     *
+     * Parameetrid puuduvad.
+     * Meetod ei tagasta midagi.
+     */
     public static void valjasta_tabel() {
         for (int i = 0; i < tabel.length; i++) {
             for (int j = 0; j < tabel[i].length; j++)
@@ -103,12 +141,21 @@ public class Mäng {
         System.out.println();
     }
 
+    /**
+     * Meetodis rakendatakse ülaltoodud meetodeid, et "käivitada" mäng.
+     *
+     * @param essamängija Mängija number 1.
+     * @param teinemängija Mängija number 2.
+     * Meetod ei tagasta midagi.
+     */
     public static void pärisMäng(Mängija essamängija, Mängija teinemängija) {
         if (!Mäng.valiAlustaja()) {
             Mängija hoia = essamängija;
             essamängija = teinemängija;
             teinemängija = hoia;
         }
+        //See kes alustab, lisab X-e.
+        //Teine mängija mängib O-dega.
         Mäng.täidaTabel();
         int kellekäik = 0;
         while (kasNeliReas() == 0) {
@@ -128,9 +175,9 @@ public class Mäng {
         }
         valjasta_tabel();
         if (kasNeliReas() == 1) {
-            System.out.println(essamängija.nimi + " võitis!");
+            System.out.println(essamängija.getNimi() + " võitis!");
         } else {
-            System.out.println(teinemängija.nimi + " võitis!");
+            System.out.println(teinemängija.getNimi() + " võitis!");
         }
 
     }
